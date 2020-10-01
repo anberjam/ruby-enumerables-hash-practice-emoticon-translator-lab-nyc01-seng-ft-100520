9f1dbd32874ca2151be8f054d4321015a7d5be2d
Anber Jamshed <anberjam@yahoo.com>
2020-10-01 19:06:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,21 @@
 # require modules here
+require 'yaml'
+emoticons= YAML.load_file('lib/emoticons.yml')
 
-def load_library
+def load_library(emoticons)
+  
+  emoticon_name= Hash.new
+  emoticons.each do |emotion, array_faces|
+    array_faces.each do |face|
+      if emoticon_name[emotion]=nil 
+        emoticon_name[emotion]={:english => face}
+      else 
+        emoticon_name[emotion][:japanese]= face
+      end
+    end
+  end
   # code goes here
+  emoticon_name
 end
 
 def get_japanese_emoticon
